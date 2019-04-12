468f97b76fce99ddff0c76fb6f1e0f8eeccbd5d7
Ricardo Molina <R3molina@.ucsd.edu>
2019-04-12 10:07:48 -0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,26 +1,167 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ricardoPic from './Images/RicardoPic.jpg';
+import richardPic from './Images/RichardPic.jpeg';
+import alexPic from './Images/AlexPic.jpg';
+import austinPic from './Images/AustinPic.jpg';
+import ericPic from './Images/EricPic.jpg';
+import gordonPic from './Images/GordonPic.jpg';
+import kylePic from './Images/KylePic.jpg';
+import philipPic from './Images/PhilipPic.JPG';
+import simonPic from './Images/SimonPic.jpeg';
+import tenshiPic from './Images/TenshiPic.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={ricardoPic} className="App-logo" alt="logo"/>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Ricardo Molina - Leader
+            <br></br>
+            Contact info 
+            <br></br>
+            Telephone: (619) 587-1009
+            <br></br>
+            Email: r3molina@ucsd.edu
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-      </div>
+      
+
+
+<header className="App-header">
+  <img src={richardPic} className="App-logo" alt="logo"/>
+  <p>
+    Richard Pena - Co Leader
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: (310) 779-5258
+    <br></br>
+    Email: penarichard53@gmail.com
+  </p>
+</header>
+
+
+
+<header className="App-header">
+  <img src={alexPic} className="App-logo" alt="logo"/>
+  <p>
+  Donghak Alex Oh - Coder
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: (619) 314-2950
+    <br></br>
+    Email: d4oh@ucsd.edu
+  </p>
+</header>
+
+
+
+<header className="App-header">
+  <img src={gordonPic} className="App-logo" alt="logo"/>
+  <p>
+  Gordon Hu - Developer Operations  
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: (425) 281-5231
+    <br></br>
+    Email: gohu@ucsd.edu
+  </p>
+</header>
+
+<header className="App-header">
+  <img src={austinPic} className="App-logo" alt="logo"/>
+  <p>
+  Austin Moss-Ennis - Quality Assurance 
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: (661) 301-0301
+    <br></br>
+    Email: amossenn@ucsd.edu
+  </p>
+</header>
+
+<header className="App-header">
+  <img src={ericPic} className="App-logo" alt="logo"/>
+  <p>
+  Eric Khoi Huynh - Developer Operations
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: (408) 550-4171
+    <br></br>
+    Email: e7huynh@ucsd.edu
+  </p>
+</header>
+
+
+<header className="App-header">
+  <img src={kylePic} className="App-logo" alt="logo"/>
+  <p>
+  Kyle Burt - Tools And Utility
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: (619) 456-7136
+    <br></br>
+    Email: k1burt@ucsd.edu
+  </p>
+</header>
+
+<header className="App-header">
+  <img src={simonPic} className="App-logo" alt="logo"/>
+  <p>
+  Simon Giraud - Coder
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: 3 (364) 761-1158
+    <br></br>
+    Email: simon.giraud007@gmail.com
+  </p>
+</header>
+
+<header className="App-header">
+  <img src={tenshiPic} className="App-logo" alt="logo"/>
+  <p>
+  Tenshi Mori - Tools And Utility  
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: (929) 276-2718
+    <br></br>
+    Email: temori@ucsd.edu
+  </p>
+</header>
+
+<header className="App-header">
+  <img src={philipPic} className="App-logo" alt="logo"/>
+  <p>
+  Phillip Jo  
+    <br></br>
+    Contact info 
+    <br></br>
+    Telephone: (714) 904-8485
+    <br></br>
+    Email: yulshimi4@gmail.com
+  </p>
+</header>
+
+
+</div>
+
+
+
+
+
+
+
     );
   }
 }
